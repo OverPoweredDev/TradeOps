30fa1d3bc73a42cc8418b1260c50a749c1672eed
Nikhil Hegde <nikhilmay2001@gmail.com>
2023-08-04 16:10:34 +0530

    [ADD] addSecurity and updateSecurity

--- back_end/src/main/java/com/group22/back_end/controllers/SecurityController.java
+++ back_end/src/main/java/com/group22/back_end/controllers/SecurityController.java
@@ -1,9 +1,28 @@
 package com.group22.back_end.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.group22.back_end.models.Security;
+import com.group22.back_end.services.SecurityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("securities/")
+@RequestMapping("/securities")
 public class SecurityController {
+
+    private SecurityService securityService;
+
+    @Autowired
+    public SecurityController(SecurityService securityService){
+        this.securityService = securityService;
+    }
+
+    @PostMapping("/add")
+    public void addSecurity(@RequestBody Security security){
+        securityService.addSecurity(security);
+    }
+
+    @PutMapping("update/")
+    public void updateSecurityById(@RequestBody Security security){
+        securityService.updateSecurity(security);
+    }
 }
--- back_end/src/main/java/com/group22/back_end/services/SecurityService.java
+++ back_end/src/main/java/com/group22/back_end/services/SecurityService.java
@@ -1,4 +1,17 @@
 package com.group22.back_end.services;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.group22.back_end.models.Security;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class SecurityService {
+
+    public void addSecurity(Security security){
+        //add logic
+    }
+
+    public void updateSecurity(Security security) {
+    }
 }
