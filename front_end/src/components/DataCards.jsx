db95f455b3a018ebce5a37a6352638dc7de2ffef
palak1202 <palakbende46979@gmail.com>
2023-08-07 18:49:17 +0530

    cards resize

--- front_end/src/components/DataCards.jsx
+++ front_end/src/components/DataCards.jsx
@@ -4,53 +4,56 @@ import tradeimg from './images/tradeimg.png';
 import securityimg from './images/securityimg.png';
 import bondimg from './images/bondimg.png'
 
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+
 class DataCards extends Component {
     render() {
         return (
             <div>
             <div className='card-deck row row-cols-1 row-cols-md-3 g-4 bg-primary d-flex justify-content-around'>
-                <div className="col w-25 p-3">
-                    <div className="card ">
+                <div className="col-sm w-25 p-3">
+                    <div className="card h-100">
                         <div className="card-body text-center">
                             <div className="row">
                                 <div className='col'>
                                     <h4 className='card-title '>Total Trades</h4>
-                                    <p class="card-text">display total trades</p>
+                                    <p className="card-text">display total trades</p>
                                 </div>
                                 <div className='col'>
-                                    <img class="card-img-right img-fluid w-25" src={tradeimg} alt="Card image cap" />
+                                    <img className="card-img-right img-fluid w-25" src={tradeimg} alt="Card image cap" />
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="col w-25 p-3">
-                    <div className="card ">
+                <div className="col-sm w-25 p-3">
+                    <div className="card h-100">
                         <div className="card-body text-center">
                         <div className="row d-flex justify-content-around">
                                 <div className='col'>
                                     <h4 className='card-title '>Total Securites</h4>
-                                    <p class="card-text">display total securities</p>
+                                    <p className="card-text">display total securities</p>
                                 </div>
                                 <div className='col w-50'>
-                                    <img class="card-img-right img-fluid w-25" src={securityimg} alt="Card image cap"></img>
+                                    <img className="card-img-right img-fluid w-25" src={securityimg} alt="Card image cap"></img>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="col w-25 p-3">
-                    <div className="card ">
+                <div className="col-sm w-25 p-3">
+                    <div className="card h-100">
                         <div className="card-body text-center">
                         <div className="row d-flex justify-content-around">
                                 <div className='col w-50'>
-                                    <h4 className='card-title '>Post Matured Bonds</h4>
-                                    <p class="card-text">display bonds</p>
+                                    <h4 className='card-title'>Post Matured Bonds</h4>
+                                    <p className="card-text">display bonds</p>
                                 </div>
                                 <div className='col w-50'>
-                                    <img class="card-img-right img-fluid w-25" src={bondimg} alt="Card image cap"></img>
+                                    <img className="card-img-right img-fluid w-25" src={bondimg} alt="Card image cap"></img>
                                 </div>
                             </div>
                         </div>
