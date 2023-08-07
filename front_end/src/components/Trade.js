7cef504a0857f7baf96f33f4111d5154af5cca57
Pooja <pujagulhane2001@gmail.com>
2023-08-07 13:18:17 +0530

    Added index.js

---
+++ front_end/src/components/Trade.js
@@ -0,0 +1,104 @@
+import React from 'react';
+
+function Trade(props) {
+    return (
+    <div className="container mt-5">
+      
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>BookId</th>
+              <th>CounterPartyId</th>
+              <th>SecurityId</th>
+              <th>Quantity</th>
+              <th>Status</th>
+              <th>Price</th>
+              <th>Buy_Sell</th>
+              <th>TradeDate</th>
+              <th>SettlementDate</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>25</td>
+              <td>111907034</td>
+              <td>XY2344</td>
+              <td>1</td>
+              <td></td>
+              <td>John Doe</td>
+              <td>25</td>
+              <td>New York</td>
+              <td>New York</td>
+
+            </tr>
+
+            <tr>
+              <td>2</td>
+              <td>25</td>
+              <td>New York</td>
+              <td>John Doe</td>
+              <td>25</td>
+              <td>New York</td>
+              <td>John Doe</td>
+              <td>25</td>
+              <td>New York</td>
+              <td>New York</td>
+            </tr>
+            <tr>
+            <td>3</td>
+              <td>25</td>
+              <td>New York</td>
+              <td>John Doe</td>
+              <td>25</td>
+              <td>New York</td>
+              <td>John Doe</td>
+              <td>25</td>
+              <td>New York</td>
+              <td>New York</td>
+              </tr>
+              <tr>
+                <td>4</td>
+                <td>25</td>
+              <td>New York</td>
+              <td>John Doe</td>
+              <td>25</td>
+              <td>New York</td>
+              <td>John Doe</td>
+              <td>25</td>
+              <td>New York</td>
+              <td>New York</td>
+              </tr>
+              <tr>
+                <td>5</td>
+                <td>25</td>
+              <td>New York</td>
+              <td>John Doe</td>
+              <td>25</td>
+              <td>New York</td>
+              <td>John Doe</td>
+              <td>25</td>
+              <td>New York</td>
+              <td>New York</td>
+              </tr>
+              <tr>
+                <td>5</td>
+                <td>25</td>
+              <td>New York</td>
+              <td>John Doe</td>
+              <td>25</td>
+              <td>New York</td>
+              <td>John Doe</td>
+              <td>25</td>
+              <td>New York</td>
+              <td>New York</td>
+              </tr>
+            {/* Add more rows here */}
+          </tbody>
+        </table>
+      </div>
+    );
+}
+
+export default Trade;
