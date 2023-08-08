369f03088998e23107f54d57831d296d4869f60d
OverPoweredDev <omkar.prabhune19@vit.edu>
2023-08-08 21:35:49 +0530

    added LoginService

---
+++ back_end/src/main/java/com/group22/back_end/services/LoginService.java
@@ -0,0 +1,52 @@
+package com.group22.back_end.services;
+
+import org.springframework.stereotype.Service;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+@Service
+public class LoginService {
+    Map<String, String> mockPasswordList = new HashMap<>();
+    LoginService() {
+        mockPasswordList.put("admin", "password");
+    }
+    public boolean checkLoginDetails(String username, String password) {
+        if(!mockPasswordList.containsKey(username)) {
+            return false;
+        }
+
+        String encryptedPassword = bytesToHex(digest(password.getBytes(UTF_8), "SHA-256"));
+        if(Objects.equals(mockPasswordList.get(username), encryptedPassword)) {
+            System.out.println(encryptedPassword);
+            return true;
+        }
+
+        return false;
+    }
+
+    public static byte[] digest(byte[] input, String algorithm) {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance(algorithm);
+        } catch (
+                NoSuchAlgorithmException e) {
+            throw new IllegalArgumentException(e);
+        }
+        byte[] result = md.digest(input);
+        return result;
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+}
